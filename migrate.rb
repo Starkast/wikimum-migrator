39d612580541849855ed7e548b634db0f04228fa
Johan Eckerström <johan@duh.se>
2014-05-29 11:09:03 +0200

    Migration for pages
    Migrates all fields currently available in
    the new wikimum code.
    
    Missing fields:
    
    - revision
    - lock_version
    - created_by
    - updated_by

--- migrate.rb
+++ migrate.rb
@@ -5,8 +5,18 @@ NEW_DB = Sequel.connect(ENV.fetch('NEW_DB_URL'))
 
 migrate_page = proc do |page|
   NEW_DB[:pages] << {
-    title: page[:title].force_encoding('UTF-8'),
-    slug: page[:shorthand_title].force_encoding('UTF-8')
+    title:                page[:title].force_encoding('UTF-8'),
+    slug:                 page[:shorthand_title].force_encoding('UTF-8'),
+    title_char:           page[:title_char].force_encoding('UTF-8'),
+    content:              page[:content].force_encoding('UTF-8'),
+    compiled_content:     page[:compiled_content].force_encoding('UTF-8'),
+    comment:              page[:comment].force_encoding('UTF-8'),
+    compiled_comment:     page[:compiled_comment].force_encoding('UTF-8'),
+    description:          page[:description].force_encoding('UTF-8'),
+    compiled_description: page[:description].force_encoding('UTF-8'),
+    created_on:           page[:created_on],
+    updated_on:           page[:updated_on],
+    markup:               page[:markup],
   }
 end
 
