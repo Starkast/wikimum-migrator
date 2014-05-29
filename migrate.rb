5868e57ec3d1710f9fea67eee21e72464110860b
Johan Eckerström <johan@duh.se>
2014-05-29 10:53:31 +0200

    Migrate page title and slug

--- migrate.rb
+++ migrate.rb
@@ -1,7 +1,15 @@
 require 'sequel'
 
-OLD_DB = Sequel.connect(ENV.fetch('OLD_DB_URL'))
+OLD_DB = Sequel.connect(ENV.fetch('OLD_DB_URL'), encoding: 'latin1')
 NEW_DB = Sequel.connect(ENV.fetch('NEW_DB_URL'))
 
-p OLD_DB.tables
-p NEW_DB.tables
+migrate_page = proc do |page|
+  NEW_DB[:pages] << {
+    title: page[:title].force_encoding('UTF-8'),
+    slug: page[:shorthand_title].force_encoding('UTF-8')
+  }
+end
+
+NEW_DB.transaction do
+  OLD_DB[:pages].each(&migrate_page)
+end
